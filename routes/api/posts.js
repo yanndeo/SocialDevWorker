b8c6e22a3d47cb69a26e158dc0791a065147a79e
Yanndeo <yan2sambou@gmail.com>
2019-05-11 11:39:48 +0000

    Creation du Post model et Ajout d'un newPost en BD --back

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,14 +1,54 @@
 const express = require('express');
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const { check, validationResult } = require("express-validator/check");
+
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+const Post = require("../../models/Post");
 
 
 
+// @route    POST api/posts
+// @desc     Create a Post
+// @access   Private
+router.post('/',[auth, 
+    [
+        check('text', 'Text is required').not().isEmpty()
+    ]
+],
+async (req, res) => {
 
-// @route    GET api/post
-// @desc     Test Route
-// @access   Public
-router.get('/', (req, res) => {
-    res.send('post route');
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+    }
+
+    try{
+
+
+        const user = await User.findById(req.user.id).select('-password');
+
+        const newPost = new Post({
+            text: req.body.text,
+            name: user.name,
+            avatar: user.avatar,
+            user: req.user.id
+        });
+
+        const post = await newPost.save(newPost);
+
+        res.json(post);
+
+        
+      
+    }catch(err){
+        console.log('post_save', err.message);
+        res.status(500).send('Server Error')
+    }
+
+
 });
 
 module.exports = router;
